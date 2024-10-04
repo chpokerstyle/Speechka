f9fe1370f3843dd006870001b021fe3a95d2db9b
Dmitry Volokitin <dvolokitin@dev.vtb.ru>
2024-10-04 21:40:20 +0300

    init

---
+++ src/main/java/ru/business_sausage/speechka/SpeechkaApplication.java
@@ -0,0 +1,13 @@
+package ru.business_sausage.speechka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeechkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeechkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/business_sausage/speechka/SpeechkaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.business_sausage.speechka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeechkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
